fc516b69197f21bd532f5b2454a44304d55aeea4
GrassyDev <81579329+GrassyDev@users.noreply.github.com>
2022-12-17 17:34:39 -0300

    tidying

--- src/main/java/io/github/GrassyDev/pvzmod/registry/entity/zombies/zombietypes/PvZombieEntity.java
+++ src/main/java/io/github/GrassyDev/pvzmod/registry/entity/zombies/zombietypes/PvZombieEntity.java
@@ -1,39 +1,17 @@
 package io.github.GrassyDev.pvzmod.registry.entity.zombies.zombietypes;
 
-import com.google.common.collect.ImmutableList;
 import io.github.GrassyDev.pvzmod.PvZCubed;
 import io.github.GrassyDev.pvzmod.registry.PvZEntity;
-import io.github.GrassyDev.pvzmod.registry.entity.plants.plantentity.lilypad.LilyPadEntity;
-import io.github.GrassyDev.pvzmod.registry.entity.plants.plantentity.peashooter.PeashooterEntity;
 import io.github.GrassyDev.pvzmod.registry.entity.zombies.miscentity.duckytube.DuckyTubeEntity;
 import io.github.GrassyDev.pvzmod.registry.entity.zombies.zombieentity.gargantuar.modernday.GargantuarEntity;
-import io.github.GrassyDev.pvzmod.registry.entity.zombies.zombieentity.imp.modernday.ImpEntity;
-import io.github.GrassyDev.pvzmod.registry.entity.zombies.zombieentity.polevaulting.PoleVaultingEntity;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.brain.Activity;
-import net.minecraft.entity.ai.brain.Brain;
-import net.minecraft.entity.ai.brain.task.LookAroundTask;
-import net.minecraft.entity.ai.brain.task.StayAboveWaterTask;
-import net.minecraft.entity.ai.brain.task.WanderAroundTask;
-import net.minecraft.entity.ai.brain.task.WardenLookAtEntityTask;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.entity.ai.goal.MoveToTargetPosGoal;
 import net.minecraft.entity.ai.pathing.*;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.warden.WardenEntity;
-import net.minecraft.entity.passive.GolemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
 
 public abstract class PvZombieEntity extends HostileEntity {
 	private int despawnDucky;
@@ -90,12 +68,11 @@ public abstract class PvZombieEntity extends HostileEntity {
 		if (!this.isSubmergedInWater() && !this.hasVehicle()){
 			if (this.isTouchingWater() && ++this.spawnDucky >= 1) {
 				if (world instanceof ServerWorld) {
-					ServerWorld serverWorld = (ServerWorld) world;
 					DuckyTubeEntity duckyTube = new DuckyTubeEntity(PvZEntity.DUCKYTUBE, this.world);
 					duckyTube.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.bodyYaw, 0.0F);
 					world.spawnEntity(duckyTube);
 					this.startRiding(duckyTube);
-					world.playSound((PlayerEntity) null, duckyTube.getX(), duckyTube.getY(), duckyTube.getZ(), SoundEvents.ENTITY_PLAYER_SPLASH, SoundCategory.BLOCKS, 0.33f, 0.8F);
+					world.playSound(null, duckyTube.getX(), duckyTube.getY(), duckyTube.getZ(), SoundEvents.ENTITY_PLAYER_SPLASH, SoundCategory.BLOCKS, 0.33f, 0.8F);
 				}
 			}
 		}
