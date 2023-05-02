99e029706c083dd77d158f967decb0ab26ebc49d
GrassyDev <>
2023-05-01 22:18:19 -0300

    stuff

--- src/main/java/io/github/GrassyDev/pvzmod/registry/entity/statuseffects/Ice.java
+++ src/main/java/io/github/GrassyDev/pvzmod/registry/entity/statuseffects/Ice.java
@@ -6,13 +6,20 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
 
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static io.github.GrassyDev.pvzmod.PvZCubed.MOD_ID;
+
 public class Ice extends StatusEffect {
     public Ice() {
         super(
                 StatusEffectType.HARMFUL, // whether beneficial or harmful for entity
                 0x33FFFF); // color in RGB
-		addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.075, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
-		addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, "22653B89-116E-49DC-9B6B-9971489B5BE5", -0.25, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
+		final UUID MAX_SPEED_UUID = UUID.nameUUIDFromBytes(MOD_ID.getBytes(StandardCharsets.UTF_8));
+		final UUID MAX_STRENGTH_UUID = UUID.nameUUIDFromBytes(MOD_ID.getBytes(StandardCharsets.UTF_8));
+		addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, String.valueOf(MAX_SPEED_UUID), -0.115375, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
+		addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE, String.valueOf(MAX_STRENGTH_UUID), -0.25, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     // This method is called every tick to check whether it should apply the status effect or not
